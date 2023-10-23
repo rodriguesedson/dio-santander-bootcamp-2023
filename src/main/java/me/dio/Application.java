26a9dd426392f8f589285bf18fa0f49a529a3a72
Edson Rodrigues <edson091990@gmail.com>
2023-10-22 23:56:30 -0300

    Second commit

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -3,11 +3,12 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
-public class SantanderBootcamp2023Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderBootcamp2023Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package me.dio.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import me.dio.domain.Repository.UserRepository;
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User Create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+    
+
+
+}
